235a6f052581cdfee22a7f69eedd6e1847088ce4
00016426 <00016426@gmail.com>
2024-03-11 17:03:43 +0500

    project started serving html

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 // express web app instance
-const express = require('express')
+const express = require('express');
 
 // parse request body to json
-const body_parser = require('body-parser')
+const bodyParser = require('body-parser');
 
 // for File IO
-const path = require('path')
+const path = require('path');
+
+// for web routing
+const web_route = require('./routes/web'); // corrected line
 
 // make mock database (raw .json file) available globally in app
 global.mock_db = path.join(__dirname, './data/mock_db.json');
 
 const app = express();
 
-app.get('/', function(req, res){
-    return res.json({
-        message: 'hello world'
-    })
+// Set the view engine for web routes
+app.set('view engine', 'pug');
 
-})
+app.use('/css', express.static('public/css'));
+app.use('/js', express.static('public/js'));
+
+app.use('/', web_route); // web routes
 
 const port = 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
 
 
 
